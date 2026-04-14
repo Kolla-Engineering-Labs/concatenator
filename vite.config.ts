91b1da2d63be8bbe89a2fcc530e2cfd91ab455ec
Christopher Vrooman <cdvrooman@gmail.com>
2026-04-13 21:37:50 -0300

    fix(vite): add authentication for codecovVitePlugin

--- vite.config.ts
+++ vite.config.ts
@@ -51,6 +51,9 @@ export default defineConfig(({ mode }) => {
       codecovVitePlugin({
         enableBundleAnalysis: env.CI === 'true',
         bundleName: 'concatenator-bundle',
+        oidc: {
+          useGitHubOIDC: true, 
+        },
       }),
     ],
     define: {
